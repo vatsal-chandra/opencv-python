0d514a1ed14aa69e8b3702800edf202844f347eb
vatsal-chandra <46576602+vatsal-chandra@users.noreply.github.com>
2019-05-25 00:39:55 +0530

    Update opencv6.py

--- opencv6.py
+++ opencv6.py
@@ -3,14 +3,14 @@ import numpy as np
 
 vid=cv2.VideoCapture(0)
 while True:
-    ret,frame=vid.read()
+    ret,frame=vid.read()     #input as original video
     hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    low=np.array([150,50,0])
-    high=np.array([255,255,255])
+    low=np.array([150,50,0])     #lower limit of hue
+    high=np.array([255,255,255])   #upper limit of hue
 
 
-    search= cv2.inRange(hsv,low,high)
-    final= cv2.bitwise_and(frame,frame,mask=search)
+    search= cv2.inRange(hsv,low,high)    #filter
+    final= cv2.bitwise_and(frame,frame,mask=search)   #final result
 
     cv2.imshow('frame',frame)
     cv2.imshow('mask',search)
